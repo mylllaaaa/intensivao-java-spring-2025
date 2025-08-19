6affcd1197152a7e23913cfe5e96bc0a210dbd85
Myllena Medeiros <myllenamed19@gmail.com>
2025-08-19 16:20:40 -0300

    GET lists

--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,13 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
-import com.devsuperior.dslist.repositories.GameRepositoty;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
 	
 	@Autowired
-	private GameRepositoty gameRepository;
+	private GameRepository gameRepository;
 
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
